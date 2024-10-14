47d6f4ee02cf871316c25a355cd3add33fd0c8a8
Omar Garcia <81117426+Josmar-o@users.noreply.github.com>
2024-10-13 22:23:30 -0500

    Modularizando

---
+++ js/cargarReutilizable.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Load navbar
+    fetch('reutilizable/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    // Load footer
+    // fetch('reutilizable/footer.html')
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         document.getElementById('footer').innerHTML = data;
+    //     });
+});
