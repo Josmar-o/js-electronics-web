3b23b82fe8a00a4d96e339dc813203a194b4aa39
Omar Garcia <81117426+Josmar-o@users.noreply.github.com>
2024-10-20 23:28:34 -0500

    Update

--- js/cargarReutilizable.js
+++ js/cargarReutilizable.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     // Load navbar
-    fetch('reutilizable/navbar.html')
+    fetch('../reutilizable/navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
